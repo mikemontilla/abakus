b7c858376db01a2804be79a6d5cfe26337364eba
Miguel Ramos <miguel9ramos@gmail.com>
2016-07-01 13:20:50 -0500

    Connection options were changed to work with mySql in production

--- build/persistence/connection.js
+++ build/persistence/connection.js
@@ -11,9 +11,10 @@ var _mysql2 = _interopRequireDefault(_mysql);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var connectionOptions = {
-    host: "localhost",
-    user: "root",
-    password: "@MorionMysql2016",
+    port: process.env.OPENSHIFT_MYSQL_DB_PORT || 3306,
+    host: process.env.OPENSHIFT_MYSQL_DB_HOST || "127.0.0.1",
+    user: process.env.NODE_ENV === "production" ? "adminrIe5g14" : "root",
+    password: process.env.NODE_ENV === "production" ? "KU2SnnwC2T65" : "@MorionMysql2016",
     database: "abakus"
 };
 
--- server/persistence/connection.js
+++ server/persistence/connection.js
@@ -1,9 +1,10 @@
 import mysql from 'mysql'
 
 const connectionOptions = {
-    host: "localhost",
-    user: "root",
-    password: "@MorionMysql2016",
+    port: (process.env.OPENSHIFT_MYSQL_DB_PORT || 3306),
+    host: (process.env.OPENSHIFT_MYSQL_DB_HOST || "127.0.0.1"),
+    user: (process.env.NODE_ENV === "production" ? "adminrIe5g14" : "root"),
+    password: (process.env.NODE_ENV === "production" ? "KU2SnnwC2T65" : "@MorionMysql2016"),
     database: "abakus"
 };
 
